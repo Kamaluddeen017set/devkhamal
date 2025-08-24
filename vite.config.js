e1362c0ff846467d6577b150ce6389af5d0b05d0
Kamaluddin Aliyu Ahmad <kamaluddeenaliyuahmad017@gmail.com>
2025-08-24 19:25:57 +0100

    redeploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// export default defineConfig({
+//   base: "/devkhamal/",
+//   plugins: [react()],
+// });
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/devkhamal/",
   plugins: [react()],
+  base: "/devkhamal/", // 👈 important! use your repo name
 });
